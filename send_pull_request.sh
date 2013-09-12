755a5216df34380d7b5723c7f58d9bcefa67159e
Marcos Diez <marcos.diez@3smobile.com>
2013-09-12 22:50:46 +0200

    we now fetch TARGET_REPO_OWNER from github if necessary

--- send_pull_request.sh
+++ send_pull_request.sh
@@ -17,15 +17,16 @@
 # in all copies or substantial portions of the Software.
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-if [ -z "$2" ]
+if [ -z "$1" ]
 then
     echo "usage: $0 YOUR_BRANCH TARGET_REPO_OWNER [PULL_REQUEST_TITLE] [PULL_REQUEST_BODY] [TARGET_REPO_BRANCH]"
+    echo "if not specified, TARGET_REPO_OWNER will be the default from GitHub"
     echo "if not specified, PULL_REQUEST_TITLE will be YOUR_BRANCH"
     echo "if not specified, PULL_REQUEST_BODY  will be blank"
     echo "if not specified, TARGET_REPO_BRANCH will be master"
@@ -53,20 +54,28 @@ else
     PULL_REQUEST_TITLE="$3"
 fi
 
-#todo GET /repos/:user/:repo in order to get TARGET_REPO_OWNER automatically
-#todo 
+
 TARGET_REPO_OWNER=$2
 YOUR_BRANCH=$1  # `git status |head -n 1  | cut -d" " -f4`
 
 LOGIN=`git config --get remote.origin.url |cut -d ":" -f2 | cut -d "/" -f 1`
 REPOSITORY=`git config --get remote.origin.url |cut -d ":" -f2 | cut -d "/" -f 2 | cut -d "." -f  1`
+AUTH=$LOGIN
+
 echo "LOGIN=${LOGIN}"
 echo "REPOSITORY=${REPOSITORY}"
 echo "TARGET_REPO_OWNER=${TARGET_REPO_OWNER}"
 echo "TARGET_REPO_BRANCH=${TARGET_REPO_BRANCH}"
 echo "YOUR_BRANCH=${YOUR_BRANCH}"
-
-curl -u ${LOGIN} "https://api.github.com/repos/${TARGET_REPO_OWNER}/${REPOSITORY}/pulls" --data "{ \"title\":\"${PULL_REQUEST_TITLE}\" , \"body\":\"${PULL_REQUEST_BODY}\" , \"head\":\"${LOGIN}:${YOUR_BRANCH}\" , \"base\": \"${TARGET_REPO_BRANCH}\"  }"
+if [ -z "$2" ]
+    then
+    read -s -p "Enter your GitHub Password: " mypassword
+    echo ""
+    AUTH="${AUTH}:${mypassword}"
+    TARGET_REPO_OWNER=`curl -u ${AUTH} "https://api.github.com/repos/${LOGIN}/${REPOSITORY}" | grep -A 50 "parent" | grep login | head -n 1 | cut -d'"' -f4`
+    echo "TARGET_REPO_OWNER=${TARGET_REPO_OWNER}"
+fi
+curl -u ${AUTH} "https://api.github.com/repos/${TARGET_REPO_OWNER}/${REPOSITORY}/pulls" --data "{ \"title\":\"${PULL_REQUEST_TITLE}\" , \"body\":\"${PULL_REQUEST_BODY}\" , \"head\":\"${LOGIN}:${YOUR_BRANCH}\" , \"base\": \"${TARGET_REPO_BRANCH}\"  }"
 
 
 
